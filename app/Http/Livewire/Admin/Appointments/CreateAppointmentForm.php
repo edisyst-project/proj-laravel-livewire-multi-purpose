fa3ea4fb13bd4daece93dce618e5134ac67a3565
edisyst <editestar@gmail.com>
2021-12-08 21:38:03 +0100

    DONE: Appointment Create - TODO: refactoring

--- app/Http/Livewire/Admin/Appointments/CreateAppointmentForm.php
+++ app/Http/Livewire/Admin/Appointments/CreateAppointmentForm.php
@@ -4,21 +4,35 @@ namespace App\Http\Livewire\Admin\Appointments;
 
 use App\Models\Appointment;
 use App\Models\Client;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class CreateAppointmentForm extends Component
 {
-    public $state = [];
+    public $state = [
+        'status' => "SCHEDULED",
+    ];
 
 
     public function createAppointment()
     {
-        dd($this->state);
-        //validate
+        $data = Validator::make(
+            $this->state,
+            [
+                'client_id' => ['required'],
+                'date'      => ['required'],
+                'time'      => ['required'],
+                'status'    => ['required', 'in:SCHEDULED,CLOSED'],
+                'note'      => ['nullable'],
+            ],[
+                'client_id.required' => 'YOU HAVE TO SELECT A CLIENT!!!!!!',
+        ])->validate();
 
-        $this->state['status'] = 'open';
+//        dd($this->state);
 
         Appointment::create($this->state);
+
+
 
         $this->dispatchBrowserEvent('alert', ['message' => 'Appointment created successfully!']);
 
--- resources/views/components/datepicker.blade.php
+++ resources/views/components/datepicker.blade.php
@@ -1,9 +1,9 @@
-@props(['id'])
+@props(['id', 'error'])
 
 
 <input {{ $attributes }}
        type="text"
-       class="form-control datetimepicker-input"
+       class="form-control datetimepicker-input @error($error) is-invalid @enderror"
        id="{{ $id }}"
        data-toggle="datetimepicker"
        data-target="#{{ $id }}"
